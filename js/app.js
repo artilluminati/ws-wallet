2f5007adec8b4e6331367d1d8143ad56d927d0cf
artilluminati <artilluminatigd@gmail.com>
2024-01-23 16:47:07 +0300

    alt

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@ $(document).ready(()=>{
 
     operations = localStorage.getItem('opHistory') ? JSON.parse(localStorage.getItem('opHistory')) : [];
 
+    $(`#filter-type option:nth-child(${user.filters.type})`).prop('selected');
+    $(`#filter-category option:nth-child(${user.filters.category})`).prop('selected');
+    $(`#filter-period option:nth-child(${user.filters.period})`).prop('selected');
+
     message.hide();
 
     if (checkAuth()){
@@ -35,6 +39,7 @@ $(document).ready(()=>{
 renderLogin = () => {
     main =`
     <div class="container login-form out">
+        <h2>Вход в аккаунт</h2>
         <input type="password" id="login-password" class="in" placeholder="Пароль">
         <button onclick="loginAuth()" class="out">Войти</button>
     </div>
@@ -52,52 +57,65 @@ renderMain = () => {
 
     main = `
         <main>
-            <header>
-                <span>Текущий баланс</span>
-                <h2>1000 ₽</h2>
-                <button onclick="renderAccount()">Настройки аккаунта</button>
-            </header>
-            <div class="container">
-                <h4>Рекомендации</h4>
-                <p>Советуем сократить расходы в категории <span id="recs-title">${categories[findMaxSumCategory(operations)[0]]}</span></p>
-                <p>Вы потратили в ней <span id="recs-sum">${findMaxSumCategory(operations)[1]} ₽</span></p>
-            </div>
-            <div class="container">
-                <button onclick="renderAddOperation()">Добавить операцию</button>
-                <button onclick="renderAddCategory()">Добавить категорию</button>
-            </div>
-            <div class="stats">
-                <div class="stats-filter">
-                    <select id="filter-type" onchange="filterUpdate()">
-                        <option value="0">Все</option>
-                        <option value="1">Расходы</option>
-                        <option value="2">Доходы</option>
-                    </select>
-                    Категория
-                    <select id="filter-category" onchange="filterUpdate()">
-                        ${catList}
-                    </select>
-                    Период
-                    <select id="filter-period" onchange="filterUpdate()">
-                        <option value="0">Все время</option>
-                        <option value="1">Месяц</option>
-                        <option value="2">Неделя</option>
-                        <option value="3">День</option>
-                    </select>
+            <section>
+                <header>
+                    <span>Текущий баланс</span>
+                    <h2>${getBalance()} ₽</h2>
+                    <button onclick="renderAccount()" class="white-btn">Настройки аккаунта</button>
+                </header>
+                <div class="container recommends out">
+                    <h4>Рекомендации</h4>
+                    <p>Советуем сократить расходы в категории <span id="recs-title">${categories[findMaxSumCategory(operations)[0]]}</span></p>
+                    <p>Вы потратили в ней <span id="recs-sum">${findMaxSumCategory(operations)[1]} ₽</span></p>
                 </div>
-                <div class="stats-history">
-                    ${statsList}
+                <div class="container additions">
+                    <button onclick="renderAddOperation()">Добавить операцию</button>
+                    <button onclick="renderAddCategory()">Добавить категорию</button>
                 </div>
-            </div>
+            </section>
+            <section>
+                <div class="stats">
+                    <div class="stats-filter">
+                        <select id="filter-type" onchange="filterUpdate()">
+                            <option value="0">Все</option>
+                            <option value="1">Расходы</option>
+                            <option value="2">Доходы</option>
+                        </select>
+                        Категория
+                        <select id="filter-category" onchange="filterUpdate()">
+                            ${catList}
+                        </select>
+                        Период
+                        <select id="filter-period" onchange="filterUpdate()">
+                            <option value="0">Все время</option>
+                            <option value="1">Месяц</option>
+                            <option value="2">Неделя</option>
+                            <option value="3">День</option>
+                        </select>
+                    </div>
+                    <div class="stats-history">
+                        <h2 style="margin-top: 40px">История операций</h2>
+                        ${statsList}
+                    </div>
+                </div>
+            </section>
         </main>
     `;
     root.html(main);
 }
 
 renderAddOperation = () => {
+    catList = '';
+    categories.forEach((element, id) => {
+        if (id>0){
+            catList += `<option value="${id}">${element}</option>`;
+        }
+    });
+
     main = `
     <div class="container">
         <button onclick="renderMain()">Назад</button>
+        <h3 style="margin-top: 10px">Новая операция</h3>
         <div class="op-stats">
             <select id="op-type" required>
                 <option value="1">Расходы</option>
@@ -105,8 +123,7 @@ renderAddOperation = () => {
             </select>
             Категория
             <select id="op-category" required>
-                <option value="1">Переводы</option>
-                <option value="2">Покупки</option>
+                ${catList}
             </select>
             Дата
             <input type="date" id="op-date" required>
@@ -125,8 +142,8 @@ renderAddCategory = () => {
     main = `
     <div class="container">
         <button onclick="renderMain()">Назад</button>
-        Новая категория
-        <input type="text" id="new-category">
+        <h3 style="margin-top: 10px">Новая категория</h3>
+        <input type="text" id="new-category" placeholder="Название">
         <button onclick="addCategory()">Добавить</button>
     </div>
     `;
@@ -136,14 +153,17 @@ renderAddCategory = () => {
 renderAccount = () => {
     main = `
     <div class="container">
-            <button onclick="renderMain()">Назад</button>
-            <h3>Настройки аккаунта</h3>
+        <button onclick="renderMain()">Назад</button>
+        <h3 style="margin-top: 10px">Настройки аккаунта</h3>
+        <div class="change-password">
             <input type="password" id="change-password" placeholder="Изменить пароль">
             <button onclick="changePassword()">Изменить</button>
-
+        </div>
+        <div>
             Выйти из аккаунта
             <button onclick="logout()">Выйти</button>
         </div>
+    </div>
     `
     root.html(main);
 }
@@ -154,9 +174,11 @@ renderHistory = (isReturn) => {
     
     statsList = '';
     filteredOps.forEach((element, id)=>{
-        statsList += `<div class="card ${element.type==1 ? 'expenses' : 'incomes'}">
-                        <span>${categories[element.category]}</span>
-                        <span>${element.sum} ₽</span>
+        statsList += `<div class="card out ${element.type==1 ? 'expenses' : 'incomes'}">
+                        <div>
+                            <span>${categories[element.category]}</span>
+                            <span>${element.sum} ₽</span>
+                        </div>
                     </div>`
     });
 
@@ -165,6 +187,20 @@ renderHistory = (isReturn) => {
     }
     $('.stats-history').html(statsList);
 }
+
+getBalance = () => {
+    sum = 0;
+    operations.forEach((element)=>{
+        if (element.type == '1'){
+            sum -= parseInt(element.sum);
+        }
+        else if(element.type == '2'){
+            sum += parseInt(element.sum);
+        }
+    });
+
+    return sum;
+};
 
 filterOps = (data) => {
     return data.filter((element)=>{
@@ -215,7 +251,6 @@ checkPeriod = (date, period) =>{
     monthDate = new Date(curDate.getTime() - 30 * 24 * 60 * 60 * 1000)
     switch (period) {
         case '3':
-            console.log(dayDate, inpDate, curDate)
             if(dayDate <= inpDate){
                 return true;
             }
@@ -252,12 +287,13 @@ addOperation = ()=>{
         sum:$('#op-sum').val(),
         date:$('#op-date').val(),
     });
-    console.log(operations);
+    animateMessage('Добавлена операция', 5000, true);
 }
 
 addCategory = ()=>{
     if ($('#new-category').val() != ''){
         categories.push($('#new-category').val());
+        animateMessage('Добавлена категория', 5000, true);
     }
 }
 
@@ -273,8 +309,15 @@ checkAuth = () => {
     return false;
 }
 
-animateMessage = async (text, time = 1000) =>{
+animateMessage = async (text, time = 1000, isOk = false) =>{
     message.html(text);
+    if (isOk){
+        message.css('color', '#96ac68!important');
+    }
+    else{
+        message.css('color', '#ac6868!important');
+    }
+       
     message.show();
     setTimeout(() => {
         message.css('animation', `hideMsg ${0.25*1000*time} ease-in`)
@@ -292,6 +335,7 @@ loginAuth = (isLogout) => {
         if ($('#login-password').val() == user.password){
             user.isAuth = true;
             renderMain();
+            animateMessage('Успешный вход', 5000, 'ok')
         }
         else{
             animateMessage('Неправильный пароль', 5000)
